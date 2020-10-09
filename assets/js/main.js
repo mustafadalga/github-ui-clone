c04040c8efeca887308e8fe9fa387506ecc51fde
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-10-10 01:37:59 +0300

    Features section completed.

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+
+// Features Section
+var features_img=document.querySelectorAll('.features-item-img img');
+var features=document.querySelectorAll('.features-item');
+
+
+function resetActive(){
+    features_img.forEach(item=>{
+        item.classList.remove("active");
+    })
+}
+function getDataId(item){
+    return item.getAttribute('data-id');
+}
+function changeImage(id){
+    features_img[id].classList.add('active');
+}
+
+features.forEach(feature=>{
+    feature.addEventListener('mouseover',()=>{
+        resetActive();
+       changeImage(getDataId(feature));
+    });
+    feature.addEventListener('mouseout',()=>{
+        resetActive();
+    });
+})
